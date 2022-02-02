f44bec8109e79f95747e0b33d341e9fec75c0c88
Rafael Sánchez Montes <rafasanchezmontes1@gmail.com>
2022-02-02 11:36:43 +0100

    i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,23 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre: string = 'Rafael';
   genero: string = 'masculino';
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente eserando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    'other': 'tenemos # clientes esperando',
   }
 
 }
