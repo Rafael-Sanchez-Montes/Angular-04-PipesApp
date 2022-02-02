abe1bab40de3f3846b7e3d9101e58b4315f87e65
Rafael Sánchez Montes <rafasanchezmontes1@gmail.com>
2022-02-02 13:57:38 +0100

    Pipes de Angular

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -19,7 +20,7 @@ export class NoComunesComponent {
 
 
   //i18nPlural
-  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientes: string[] = ['Maria', 'Pedro', 'Rafael', 'Ernesto', 'Alba'];
   clientesMapa = {
     '=0': 'no tenemos ningún cliente eserando',
     '=1': 'tenemos un cliente esperando',
@@ -36,5 +37,41 @@ export class NoComunesComponent {
   borrarCliente() {
       this.clientes.pop();
   }
+
+
+  //KeyValue Pipe
+  persona = {
+    nombre: 'Rafael',
+    edad: 35,
+    direccion: 'Cordoba, España'
+  };
+
+  //Json Pipe
+
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: false
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ];
+
+  // Async Pipe
+  miObservable = interval(2000);// 0,1,2,3,4...
+
+  valorPromesa = new Promise( (resolve, reject) => {
+
+    setTimeout( () => {
+      resolve( 'Tenemos data de promesa' );
+    }, 3500 );
+
+  });
 
 }
